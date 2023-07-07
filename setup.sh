404ed3468a462ef18bfb404e9543cdfac6f3e1d0
Çağrı Efe Günay <efe.gunay@metu.edu.tr>
2023-07-07 04:18:56 +0300

    Added setup script with pseudo, not usable

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# TODO
+
+# if os-release = Ubuntu 22.04, then run script, else break and print "Are you sure that you are using Ubuntu 22.04?"
+
+# update and upgrade the system without any output or tty window
+
+# download and install docker with get-docker.sh script
+
+# grant user to use docker with root privileges if needed
+
+# docker compose up -d --build
