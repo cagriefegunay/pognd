93e7b8f91e85d967557863525ed82d6352ee960b
Çağrı Efe Günay <efe.gunay@metu.edu.tr>
2023-07-09 01:23:08 +0300

    setup.sh::Added pseudo for generating keys

--- setup.sh
+++ setup.sh
@@ -16,6 +16,7 @@
 # rm get-docker.sh
 
 # create certificate
+# openssl req -subj '/CN=localhost' -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365
 
 # Start with docker compose
 
