d17e58238b8d7547b578c7c0c6c11014e9d1e5f1
Çağrı Efe Günay <efe.gunay@metu.edu.tr>
2023-07-08 08:03:38 +0300

    setup.sh::Added os-release checks and docker installation scripts

--- setup.sh
+++ setup.sh
@@ -3,13 +3,19 @@
 # TODO
 
 # if os-release = Ubuntu 22.04, then run script, else break and print "Are you sure that you are using Ubuntu 22.04?"
+# cat /etc/os-release ==> (ID or ID_LIKE is ubuntu) and (VERSION_ID is 22.04)
 
 # update and upgrade the system without any output or tty window
 
 # Check the required packages if they are installed. If not, install.
-# git curl uidmap
+# git curl uidmap -y
 
 # download and install docker with get-docker.sh script
+# curl -fsSL get.docker.com -o get-docker.sh
+# CHANNEL=stable sh get-docker.sh
+# rm get-docker.sh
+
+
 
 # grant user to use docker with root privileges if needed
 # docker-rootless-setuptool.sh
